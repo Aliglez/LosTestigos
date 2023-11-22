98467f93ff678a588a70110a62042ab8f9d62b7d
Stiven Diaz <steven_el22@hotmail.com>
2023-11-22 01:22:58 -0800

    modifid nav bar

---
+++ public/app.ts
@@ -0,0 +1,6 @@
+const button = (document.querySelector('.button') as HTMLButtonElement)
+const nav = (document.querySelector('.nav') as HTMLElement)
+
+button.addEventListener('click', () : void => {
+      nav.classList.toggle('activo')
+})
