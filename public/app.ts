ad5ae7f6928572ec1043440583fee6b658160889
Stiven Diaz <steven_el22@hotmail.com>
2023-11-23 21:15:05 -0800

    changed in header

--- public/app.ts
+++ public/app.ts
@@ -1,6 +1,6 @@
-const button = (document.querySelector('.button') as HTMLButtonElement)
+const button = (document.querySelector('.buton') as HTMLButtonElement)
 const nav = (document.querySelector('.nav') as HTMLElement)
 
-button.addEventListener('click', () : void => {
+button.addEventListener('click',() : void => {
       nav.classList.toggle('activo')
 })
