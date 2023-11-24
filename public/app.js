b90421703ed1886e139f89eee7b2832b2b9715bf
Stiven Diaz <steven_el22@hotmail.com>
2023-11-24 02:35:58 -0800

    changed

---
+++ public/app.js
@@ -0,0 +1,6 @@
+const button = (document.querySelector('.buton'))
+const nav = document.querySelector('.nav click');
+
+button.addEventListener('click',() => {
+      nav.classList.toggle('activo')
+})
--- public/app.ts
+++
@@ -1,6 +0,0 @@
-const button = (document.querySelector('.buton') as HTMLButtonElement)
-const nav = (document.querySelector('.nav') as HTMLElement)
-
-button.addEventListener('click',() : void => {
-      nav.classList.toggle('activo')
-})
